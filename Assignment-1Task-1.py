62ef730e24814cd3a9a8820d5dd6664523136601
Kirti Vardhini Vyas <keertivardhini@gmail.com>
2025-12-13 13:47:47 +0530

    Delete Assignment-1Task-1.py

--- Assignment-1Task-1.py
+++
@@ -1,7 +0,0 @@
-#Write a Python program that Performs the basic mathematical operations on the two numbers entered by the user.
-n1=float(input("Enter the 1st number:"))
-n2=float(input("Enter the 2nd number:"))
-print("Addition:",n1+n2)
-print("Subtraction:",n1-n2)
-print("Multiplication:",n1*n2)
-print("Division:",n1/n2)
